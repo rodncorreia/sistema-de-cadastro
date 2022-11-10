5eec6f88dceae473765d30ba509e44b68262dc20
rodnc <rodnoliveira@gmail.com>
2022-11-10 19:45:12 -0300

    prompt do console log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// prompt do console log
 console.log("editei ontem, confira as alterações pelo Visual Studio Code")
