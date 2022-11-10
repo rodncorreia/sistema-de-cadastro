fc8e461b98a94e6f68256005c32c1eb3cae39848
rodnc <rodnoliveira@gmail.com>
2022-11-10 18:34:41 -0300

    vontando para o estado anterior

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 console.log("Rodando o sistema de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("editei ontem, confira as alterações pelo Visual Studio Code")
-// mais um console log de teste:
-console.log("Enviando para o GitHub")
